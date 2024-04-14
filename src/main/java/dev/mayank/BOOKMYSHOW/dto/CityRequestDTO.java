ce29f1fc533e401512ef495f9fecadcc83a79c7a
MAYANK CHANDROL <mayankchandrol201997@gmail.com>
2024-04-15 00:28:36 +0530

    added all repository and city controller

---
+++ src/main/java/dev/mayank/BOOKMYSHOW/dto/CityRequestDTO.java
@@ -0,0 +1,10 @@
+package dev.mayank.BOOKMYSHOW.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CityRequestDTO {
+    private String name;
+}
---
+++ src/main/java/dev/mayank/BOOKMYSHOW/service/CityService.java
@@ -0,0 +1,29 @@
+package dev.mayank.BOOKMYSHOW.service;
+
+import dev.mayank.BOOKMYSHOW.models.City;
+import dev.mayank.BOOKMYSHOW.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityService {
+    @Autowired
+    private CityRepository cityRepository;
+
+    public City saveCity(String cityName)
+    {
+        City city = new City();
+        city.setName(cityName);
+        return cityRepository.save(city);
+    }
+
+    public boolean deleteCity(int cityId)
+    {
+        cityRepository.deleteById(cityId);
+        return true;
+    }
+
+    public City getCityByName(String cityName) {
+        return cityRepository.findCityByName(cityName);
+    }
+}
